433af8a4bdabf5277e2d214c486ef594ce80bc74
NekohaRin <sciencenininja9@gmail.com>
2026-05-20 12:38:49 +0700

    maaf nyerah kak

---
+++ src/Controller/EvaluasiController.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+import DAO.EvaluasiDAO;
+import Model.Evaluasi;
+import java.util.List;
+public class EvaluasiController {
+    private EvalusiDAO evaluasiDAO;
+    
+}
